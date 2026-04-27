64547c77061557ffe3292b4e43078ea6c289dffd
TommasiLarkingDavide <davide.tommasilarking@marconibari.edu.it>
2026-04-27 18:14:34 +0000

    ConsegnaD

--- Pila.java
+++ Pila.java
@@ -1,1 +1,25 @@
-//Implementare qui la classe Pila
+public class Pila<T> {
+    private Nodo<T> top;
+
+    public Pila() {
+        this.top = null;
+    }
+}
+public T peek() {
+    if (isEmpty()) {
+        throw new RuntimeException("Pila vuota");
+    }
+
+    return top.dato;
+}
+public int size() {
+    int contatore = 0;
+    Nodo<T> corrente = top;
+
+    while (corrente != null) {
+        contatore++;
+        corrente = corrente.next;
+    }
+
+    return contatore;
+}
